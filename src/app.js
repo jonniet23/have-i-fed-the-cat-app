4a4660a7185b3d731073ca919f276e388e9971e0
Jonnie <jonniet23@hotmail.com>
2023-07-12 21:10:15 +0100

    First commit and set up

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// we expect to have to parse json from request bodies, 
+// so we need the JSON middleware
+app.use(express.json());
+
+// we will put our routes and controller functions here
+
+module.exports = app;
